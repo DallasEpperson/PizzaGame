0c3acc1a0b72a16e0d202fca699bf3d99a757ab3
Dallas Epperson <depperson@pcgus.com>
2023-08-03 21:43:12 -0400

    Render hero sprite

--- Overworld.js
+++ Overworld.js
@@ -11,5 +11,23 @@ class Overworld {
             this.ctx.drawImage(image, 0, 0);
         };
         image.src = "/images/maps/DemoLower.png";
+
+        const x = 5;
+        const y = 6;
+        const hero = new Image();
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero,
+                0, //startXCut
+                0, //startYCut
+                32, //widthCut
+                32, //heightCut
+                x * 16 - 8, //posX
+                y * 16 - 18, //posY
+                32, //width
+                32 //height
+                );
+        };
+        hero.src = "/images/characters/people/hero.png";
     }
 }
