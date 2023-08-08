bab2bf898e43dfa72ef8a130898a8895899c21e4
Dallas Epperson <depperson@pcgus.com>
2023-08-07 23:03:01 -0400

    Add animation to text messages

--- Overworld.js
+++ Overworld.js
@@ -58,19 +58,14 @@ class Overworld {
     }
 
     init() {
-        this.startMap(window.OverworldMaps.DemoRoom);
+        this.startMap(window.OverworldMaps.Kitchen);
         this.bindActionInput();
         this.bindHeroPositionCheck();
         this.directionInput = new DirectionInput();
         this.directionInput.init();
         this.startGameLoop();
-        // this.map.startCutscene([
-        //     { who: "hero", type: "walk", direction: "down"},
-        //     { who: "hero", type: "walk", direction: "down"},
-        //     { who: "npcA", type: "walk", direction: "up"},
-        //     { who: "npcA", type: "walk", direction: "left"},
-        //     { who: "hero", type: "stand", direction: "right", time: 200},
-        //     {type: "textMessage", text: "Hello World!"}
-        // ]);
+        this.map.startCutscene([
+            { type: "textMessage", text: "This is the very first message!"}
+        ]);
     }
 }
